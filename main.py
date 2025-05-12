b88bd0d38a8a921c84435547f317eb5db1c1524c
Pankaj Paliwal <158048038+pankaj-0301@users.noreply.github.com>
2025-05-12 12:45:38 +0530

    commit backend

--- main.py
+++ main.py
@@ -1,7 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+from app import app  # ✅ Import the FastAPI app from app.py
